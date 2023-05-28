5e9562f98129215dc0e113bd7e2683892f3bfb94
Anastasiia Makarenko <irishmoth@yandex.ru>
2023-05-28 22:11:12 +0300

    Исправляю синтаксис в тесте

--- flow/src/androidTest/java/pro/progr/flow/CalendarListItemTest.kt
+++ flow/src/androidTest/java/pro/progr/flow/CalendarListItemTest.kt
@@ -8,6 +8,9 @@ import androidx.compose.ui.test.onNodeWithText
 import org.junit.Rule
 import org.junit.Test
 import pro.progr.flow.composable.CalendarListItem
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 class CalendarListItemTest {
@@ -18,8 +21,12 @@ class CalendarListItemTest {
     @Test
     fun calendarListItem_displays_correct_date_and_content() {
         // Arrange
-        val date = "2023-04-13"
-        val content: @Composable () -> Unit =  { Text(text = "Some content") }
+        var formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        formatter =
+            formatter.withLocale(Locale.getDefault()) // Locale specifies human language for translating, and cultural norms for lowercase/uppercase and abbreviations and such. Example: Locale.US or Locale.CANADA_FRENCH
+
+        val date = LocalDate.parse("2023-04-13", formatter)
+        val content: @Composable (d : LocalDate) -> Unit =  { Text(text = "Some content") }
 
         // Act
         composeTestRule.setContent {
@@ -27,7 +34,7 @@ class CalendarListItemTest {
         }
 
         // Assert
-        composeTestRule.onNodeWithText(text = date).assertIsDisplayed()
+        composeTestRule.onNodeWithText(text = date.toString()).assertIsDisplayed()
         composeTestRule.onNodeWithText(text = "Some content").assertIsDisplayed()
     }
 }
