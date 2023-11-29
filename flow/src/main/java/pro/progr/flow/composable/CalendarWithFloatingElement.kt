1d4fc032189db1168b319d28bf94c97f5ef6dfec
Anastasiia Makarenko <irishmoth@yandex.ru>
2023-11-29 21:04:00 +0300

    Возможность повесить элемент над календарём

---
+++ flow/src/main/java/pro/progr/flow/composable/CalendarWithFloatingElement.kt
@@ -0,0 +1,64 @@
+package pro.progr.flow.composable
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.unit.Velocity
+import pro.progr.flow.START_POSITION
+import pro.progr.flow.calendarViewModel
+import pro.progr.flow.getDateByIndex
+import pro.progr.flow.getIndex
+import pro.progr.flow.util.Scroller
+import java.time.LocalDate
+
+//todo: общие куски кода вынести отдельно
+@Composable
+fun CalendarWithFloatingElement(content: @Composable (date : LocalDate) -> Unit, floatingElement: @Composable () -> Unit) {
+
+    val verticalScrollState = rememberLazyListState(initialFirstVisibleItemIndex = START_POSITION)
+    val horizontalScrollState = rememberLazyListState(initialFirstVisibleItemIndex = START_POSITION)
+
+    val index = calendarViewModel.selectedGridIndex.collectAsState(initial = getIndex(calendarViewModel.getSelectedDate()))
+    Scroller.scroll(verticalScrollState, horizontalScrollState, index.value)
+
+
+    val nestedScrollConnectionVert = remember {
+        object : NestedScrollConnection {
+            override suspend fun onPostFling(consumed: Velocity, available: Velocity): Velocity {
+                horizontalScrollState.scrollToItem(verticalScrollState.firstVisibleItemIndex)
+                calendarViewModel.updateSelectedDate(getDateByIndex(verticalScrollState.firstVisibleItemIndex))
+                return super.onPostFling(consumed, available)
+            }
+        }
+    }
+
+    BoxWithConstraints(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        VerticalDatesList(nestedScrollConnectionVert, verticalScrollState, content)
+
+        Column(
+            modifier = Modifier.align(Alignment.BottomCenter)
+        ) {
+            floatingElement()
+
+            Box {
+                BottomCalendar(
+                    horizontalScrollState = horizontalScrollState,
+                    verticalScrollState = verticalScrollState,
+                    calendarViewModel
+                )
+            }
+
+        }
+
+    }
+}
